6405c1e29b521154f323da23c9b9547721883b68
Wilian Batista <wilian.batista.santos@gmail.com>
2023-02-02 15:55:24 -0300

    Nav e Footer

--- costs/src/App.js
+++ costs/src/App.js
@@ -3,26 +3,27 @@ import Home from './components/pages/Home'
 import Contact from './components/pages/Contact'
 import Company from './components/pages/Company'
 import NewProject from './components/pages/NewProject'
+import Projects from "./components/pages/Projects";
+
 import Container from './components/layout/Container'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/contact'>Contato</Link>
-        <Link to='/company'>Empresa</Link>
-        <Link to='/newproject'>Novo Projeto</Link>
-      </div>
-      <Container customClass='minHeight'>
+     <Navbar />
+      <Container customClass='min-height'>
         <Routes>
           <Route exact path='/' element={<Home />} />
+          <Route exact path='/projects' element={<Projects />} />
           <Route exact path='/company' element={<Company />} />
           <Route exact path='/contact' element={<Contact />} />
           <Route exact path='/newproject' element={<NewProject />} />
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer />
     </BrowserRouter>
   );
 }
